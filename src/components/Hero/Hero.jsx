b6c851e84e86cc96d7fb7332a36575cc354fd089
Darius Seals <tristarmediagroupinc@gmail.com>
2023-09-09 17:17:51 -0400

    update Hero component with content, nav and github redirect button

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { logo } from "../../assets";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center  w-full mb-10 pt-3">
+        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
+
+        <button
+          type="button"
+          className="black_btn"
+          onClick={() => {
+            window.open("https://github.com/Dseals82/ai_summarizer_app.git");
+          }}
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Ariticles width <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplify your reading with Sumz, an open-source article summarizer that
+        transforms lengthy articles into clear an concise summaries.
+      </h2>
+    </header>
+  );
 };
 
 export default Hero;
