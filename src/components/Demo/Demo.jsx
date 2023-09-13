d5a861aec39c389d5af841c2cace7a8abae3b113
Darius Seals <tristarmediagroupinc@gmail.com>
2023-09-12 23:29:15 -0400

    implement article summary feature that displays summary

--- src/components/Demo/Demo.jsx
+++ src/components/Demo/Demo.jsx
@@ -88,6 +88,32 @@ const Demo = () => {
         </div>
       </div>
       {/* Display Results */}
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+        ) : error ? (
+          <p className="font-inter font-bold text-black">
+            Nah Dawgs 😕
+            <br />
+            <span className="font-satoshi font-normal text-gray-700">
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bld text-grey-600 text-xl">
+                Article <span className="blue_gradient">Summary</span>
+                <div className="summary_box">
+                  <p className="font-inter font-medium text-sm text-gray-700">
+                    {article.summary}
+                  </p>
+                </div>
+              </h2>
+            </div>
+          )
+        )}
+      </div>
     </section>
   );
 };
